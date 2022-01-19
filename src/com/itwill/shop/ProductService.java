ebe9985c21c56f914a55f6784fab7c0bde18688a
powet <powet@172.30.1.38>
2022-01-19 14:25:57 +0900

    dd

--- src/com/itwill/shop/ProductService.java
+++ src/com/itwill/shop/ProductService.java
@@ -1,5 +1,11 @@
 package com.itwill.shop;
 
 public class ProductService {
-
+   private ProductDao productDao;
+   public MemberService() {
+	   productDao=new ProductDao();
+   }
+   public void selectAll() {
+	   
+   }
 }
